60740abd50830bee57c69ea6427783134a2a85c7
LucaPolattini <luca.polattini@mail.polimi.it>
2021-04-06 18:05:46 +0200

    sistema

---
+++ IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/CreateCards.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+public class CreateCards {
+    private DevelopmentCard[] AllCards = new DevelopmentCard[48];
+
+    AllCards[0] = new DevelopmentCard({1,2}, int[] resourcesProductionCost, int[] resourcesProductionProfit, int id, DevelopmentCard.colorCard color, int level, int victoryPoints)
+}
--- IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/GridCell.java
+++ IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/GridCell.java
@@ -7,11 +7,8 @@ public class GridCell {
     private DevelopmentCard[] cell;
     private int i = 0;
 
-    public GridCell(DevelopmentCard card1,DevelopmentCard card2,DevelopmentCard card3,DevelopmentCard card4) {
-        cell[0] = card1;
-        cell[1] = card2;
-        cell[2] = card3;
-        cell[3] = card4;
+    public GridCell(DevelopmentCard[] ArrayDevelopmentCard) {
+        cell = ArrayDevelopmentCard;
 
         List<DevelopmentCard> ListCell = Arrays.asList(cell);
         Collections.shuffle(ListCell);
