b62a8f38db4df5ea79e085d87362f014712a1c11
LucaPolattini <luca.polattini@mail.polimi.it>
2021-04-07 10:35:49 +0200

    Creazione DevelopmentCards

--- IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/CreateCards.java
+++ IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/CreateCards.java
@@ -1,7 +1,19 @@
 package it.polimi.ingsw;
 
 public class CreateCards {
-    private DevelopmentCard[] AllCards = new DevelopmentCard[48];
+    private DevelopmentCard[] Green1 = new DevelopmentCard[4];
+    private DevelopmentCard[] Green2 = new DevelopmentCard[4];
+    private DevelopmentCard[] Green3 = new DevelopmentCard[4];
+    private DevelopmentCard[] Yellow1 = new DevelopmentCard[4];
+    private DevelopmentCard[] Yellow2 = new DevelopmentCard[4];
+    private DevelopmentCard[] Yellow3 = new DevelopmentCard[4];
+    private DevelopmentCard[] Blue1 = new DevelopmentCard[4];
+    private DevelopmentCard[] Blue2 = new DevelopmentCard[4];
+    private DevelopmentCard[] Blue3 = new DevelopmentCard[4];
+    private DevelopmentCard[] Purple1 = new DevelopmentCard[4];
+    private DevelopmentCard[] Purple2 = new DevelopmentCard[4];
+    private DevelopmentCard[] Purple3 = new DevelopmentCard[4];
 
-    AllCards[0] = new DevelopmentCard({1,2}, int[] resourcesProductionCost, int[] resourcesProductionProfit, int id, DevelopmentCard.colorCard color, int level, int victoryPoints)
+
+    Green1[0] = new DevelopmentCard({1,2}, int[] resourcesProductionCost, int[] resourcesProductionProfit, int id, DevelopmentCard.colorCard color, int level, int victoryPoints)
 }
