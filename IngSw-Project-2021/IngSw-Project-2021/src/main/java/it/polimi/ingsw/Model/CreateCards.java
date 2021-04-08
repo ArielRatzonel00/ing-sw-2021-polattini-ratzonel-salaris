3fdeb0ca997346926fe5e2f3d42ea1f71045ac06
LucaPolattini <luca.polattini@mail.polimi.it>
2021-04-08 11:35:58 +0200

    Completamento Creazione DevelopmentCards

--- IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/Model/CreateCards.java
+++ IngSw-Project-2021/IngSw-Project-2021/src/main/java/it/polimi/ingsw/Model/CreateCards.java
@@ -26,45 +26,45 @@ public class CreateCards {
 
 
 
-    private DevelopmentCard Green21 = new DevelopmentCard (new int[]{0, 2, 0, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 0, 0, 0, 1}, 17, DevelopmentCard.colorCard.Green, 2, 5);
-    private DevelopmentCard Green22 = new DevelopmentCard (new int[]{1, 1, 0, 1}, new int[]{0, 0, 0, 1}, new int[]{1, 0, 0, 0, 0}, 18, DevelopmentCard.colorCard.Green, 2, 6);
-    private DevelopmentCard Green23 = new DevelopmentCard (new int[]{0, 3, 0, 0}, new int[]{2, 0, 0, 0}, new int[]{0, 1, 1, 1, 0}, 19, DevelopmentCard.colorCard.Green, 2, 7);
-    private DevelopmentCard Green24 = new DevelopmentCard (new int[]{0, 2, 2, 0}, new int[]{1, 0, 0, 1}, new int[]{0, 0, 2, 0, 1}, 20, DevelopmentCard.colorCard.Green, 2, 8);
+    private DevelopmentCard Green21 = new DevelopmentCard (new int[]{0, 4, 0, 0}, new int[]{0, 0, 0, 1}, new int[]{0, 0, 0, 0, 2}, 17, DevelopmentCard.colorCard.Green, 2, 5);
+    private DevelopmentCard Green22 = new DevelopmentCard (new int[]{2, 3, 0, 0}, new int[]{1, 1, 0, 0}, new int[]{0, 0, 0, 3, 0}, 18, DevelopmentCard.colorCard.Green, 2, 6);
+    private DevelopmentCard Green23 = new DevelopmentCard (new int[]{0, 5, 0, 0}, new int[]{0, 0, 2, 0}, new int[]{0, 0, 0, 2, 2}, 19, DevelopmentCard.colorCard.Green, 2, 7);
+    private DevelopmentCard Green24 = new DevelopmentCard (new int[]{0, 3, 3, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 2, 0, 0, 1}, 20, DevelopmentCard.colorCard.Green, 2, 8);
 
-    private DevelopmentCard Yellow21 = new DevelopmentCard (new int[]{0, 0, 0, 2}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 0, 1}, 21, DevelopmentCard.colorCard.Yellow, 2, 5);
-    private DevelopmentCard Yellow22 = new DevelopmentCard (new int[]{0, 1, 1, 1}, new int[]{0, 1, 0, 1}, new int[]{0, 0, 1, 0, 0}, 22, DevelopmentCard.colorCard.Yellow, 2, 6);
-    private DevelopmentCard Yellow23 = new DevelopmentCard (new int[]{0, 0, 0, 3}, new int[]{0, 2, 0, 0}, new int[]{1, 0, 1, 1, 0}, 23, DevelopmentCard.colorCard.Yellow, 2, 7);
-    private DevelopmentCard Yellow24 = new DevelopmentCard (new int[]{0, 2, 0, 2}, new int[]{1, 0, 1, 0}, new int[]{0, 2, 0, 0, 1}, 24, DevelopmentCard.colorCard.Yellow, 2, 8);
+    private DevelopmentCard Yellow21 = new DevelopmentCard (new int[]{0, 0, 0, 4}, new int[]{0, 1, 0, 0}, new int[]{0, 0, 0, 0, 2}, 21, DevelopmentCard.colorCard.Yellow, 2, 5);
+    private DevelopmentCard Yellow22 = new DevelopmentCard (new int[]{0, 2, 0, 3}, new int[]{0, 1, 0, 1}, new int[]{0, 0, 3, 0, 0}, 22, DevelopmentCard.colorCard.Yellow, 2, 6);
+    private DevelopmentCard Yellow23 = new DevelopmentCard (new int[]{0, 0, 0, 5}, new int[]{0, 2, 0, 0}, new int[]{2, 0, 0, 0, 2}, 23, DevelopmentCard.colorCard.Yellow, 2, 7);
+    private DevelopmentCard Yellow24 = new DevelopmentCard (new int[]{3, 0, 0, 3}, new int[]{0, 1, 0, 0}, new int[]{0, 0, 2, 0, 1}, 24, DevelopmentCard.colorCard.Yellow, 2, 8);
 
-    private DevelopmentCard Purple21 = new DevelopmentCard (new int[]{2, 0, 0, 0}, new int[]{0, 0, 0, 1}, new int[]{0, 0, 0, 0, 1}, 25, DevelopmentCard.colorCard.Purple, 2, 5);
-    private DevelopmentCard Purple22 = new DevelopmentCard (new int[]{1, 1, 1, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 1, 0, 0, 0}, 26, DevelopmentCard.colorCard.Purple, 2, 6);
-    private DevelopmentCard Purple23 = new DevelopmentCard (new int[]{3, 0, 0, 0}, new int[]{0, 0, 2, 0}, new int[]{1, 1, 0, 1, 0}, 27, DevelopmentCard.colorCard.Purple, 2, 7);
-    private DevelopmentCard Purple24 = new DevelopmentCard (new int[]{2, 0, 0, 2}, new int[]{0, 1, 1, 0}, new int[]{0, 0, 0, 2, 1}, 28, DevelopmentCard.colorCard.Purple, 2, 8);
+    private DevelopmentCard Purple21 = new DevelopmentCard (new int[]{4, 0, 0, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 0, 0, 0, 2}, 25, DevelopmentCard.colorCard.Purple, 2, 5);
+    private DevelopmentCard Purple22 = new DevelopmentCard (new int[]{3, 0, 2, 0}, new int[]{1, 0, 1, 0}, new int[]{3, 0, 0, 0, 0}, 26, DevelopmentCard.colorCard.Purple, 2, 6);
+    private DevelopmentCard Purple23 = new DevelopmentCard (new int[]{5, 0, 0, 0}, new int[]{0, 0, 0, 2}, new int[]{0, 0, 2, 0, 2}, 27, DevelopmentCard.colorCard.Purple, 2, 7);
+    private DevelopmentCard Purple24 = new DevelopmentCard (new int[]{3, 3, 0, 0}, new int[]{0, 0, 0, 1}, new int[]{2, 0, 0, 0, 1}, 28, DevelopmentCard.colorCard.Purple, 2, 8);
 
-    private DevelopmentCard Blue21 = new DevelopmentCard (new int[]{0, 0, 2, 0}, new int[]{0, 1, 0, 0}, new int[]{0, 0, 0, 0, 1}, 29, DevelopmentCard.colorCard.Blue, 2, 5);
-    private DevelopmentCard Blue22 = new DevelopmentCard (new int[]{1, 0, 1, 1}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 1, 0}, 30, DevelopmentCard.colorCard.Blue, 2, 6);
-    private DevelopmentCard Blue23 = new DevelopmentCard (new int[]{0, 0, 3, 0}, new int[]{0, 0, 0, 2}, new int[]{1, 1, 1, 0, 0}, 31, DevelopmentCard.colorCard.Blue, 2, 7);
-    private DevelopmentCard Blue24 = new DevelopmentCard (new int[]{2, 0, 2, 0}, new int[]{0, 1, 0, 1}, new int[]{2, 0, 0, 0, 1}, 31, DevelopmentCard.colorCard.Blue, 2, 8);
+    private DevelopmentCard Blue21 = new DevelopmentCard (new int[]{0, 0, 4, 0}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 0, 2}, 29, DevelopmentCard.colorCard.Blue, 2, 5);
+    private DevelopmentCard Blue22 = new DevelopmentCard (new int[]{0, 0, 3, 2}, new int[]{0, 0, 1, 1}, new int[]{3, 0, 0, 0, 0}, 30, DevelopmentCard.colorCard.Blue, 2, 6);
+    private DevelopmentCard Blue23 = new DevelopmentCard (new int[]{0, 0, 5, 0}, new int[]{2, 0, 0, 0}, new int[]{0, 2, 0, 0, 2}, 31, DevelopmentCard.colorCard.Blue, 2, 7);
+    private DevelopmentCard Blue24 = new DevelopmentCard (new int[]{0, 0, 3, 3}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 2, 1}, 31, DevelopmentCard.colorCard.Blue, 2, 8);
 
 
 
-    private DevelopmentCard Green31 = new DevelopmentCard (new int[]{0, 2, 0, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 0, 0, 0, 1}, 33, DevelopmentCard.colorCard.Green, 3, 9);
-    private DevelopmentCard Green32 = new DevelopmentCard (new int[]{1, 1, 0, 1}, new int[]{0, 0, 0, 1}, new int[]{1, 0, 0, 0, 0}, 34, DevelopmentCard.colorCard.Green, 3, 10);
-    private DevelopmentCard Green33 = new DevelopmentCard (new int[]{0, 3, 0, 0}, new int[]{2, 0, 0, 0}, new int[]{0, 1, 1, 1, 0}, 35, DevelopmentCard.colorCard.Green, 3, 11);
-    private DevelopmentCard Green34 = new DevelopmentCard (new int[]{0, 2, 2, 0}, new int[]{1, 0, 0, 1}, new int[]{0, 0, 2, 0, 1}, 36, DevelopmentCard.colorCard.Green, 3, 12);
+    private DevelopmentCard Green31 = new DevelopmentCard (new int[]{0, 6, 0, 0}, new int[]{2, 0, 0, 0}, new int[]{0, 0, 0, 3, 2}, 33, DevelopmentCard.colorCard.Green, 3, 9);
+    private DevelopmentCard Green32 = new DevelopmentCard (new int[]{2, 5, 0, 0}, new int[]{1, 0, 1, 0}, new int[]{0, 2, 0, 2, 1}, 34, DevelopmentCard.colorCard.Green, 3, 10);
+    private DevelopmentCard Green33 = new DevelopmentCard (new int[]{0, 7, 0, 0}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 1, 0, 3}, 35, DevelopmentCard.colorCard.Green, 3, 11);
+    private DevelopmentCard Green34 = new DevelopmentCard (new int[]{0, 4, 4, 0}, new int[]{0, 0, 0, 1}, new int[]{0, 1, 3, 0, 0}, 36, DevelopmentCard.colorCard.Green, 3, 12);
 
-    private DevelopmentCard Yellow31 = new DevelopmentCard (new int[]{0, 0, 0, 2}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 0, 1}, 38, DevelopmentCard.colorCard.Yellow, 3, 9);
-    private DevelopmentCard Yellow32 = new DevelopmentCard (new int[]{0, 1, 1, 1}, new int[]{0, 1, 0, 1}, new int[]{0, 0, 1, 0, 0}, 39, DevelopmentCard.colorCard.Yellow, 3, 10);
-    private DevelopmentCard Yellow33 = new DevelopmentCard (new int[]{0, 0, 0, 3}, new int[]{0, 2, 0, 0}, new int[]{1, 0, 1, 1, 0}, 40, DevelopmentCard.colorCard.Yellow, 3, 11);
-    private DevelopmentCard Yellow34 = new DevelopmentCard (new int[]{0, 2, 0, 2}, new int[]{1, 0, 1, 0}, new int[]{0, 2, 0, 0, 1}, 41, DevelopmentCard.colorCard.Yellow, 3, 12);
+    private DevelopmentCard Yellow31 = new DevelopmentCard (new int[]{0, 0, 0, 6}, new int[]{0, 2, 0, 0}, new int[]{3, 0, 0, 0, 2}, 38, DevelopmentCard.colorCard.Yellow, 3, 9);
+    private DevelopmentCard Yellow32 = new DevelopmentCard (new int[]{2, 0, 0, 5}, new int[]{1, 0, 0, 1}, new int[]{0, 2, 2, 0, 1}, 39, DevelopmentCard.colorCard.Yellow, 3, 10);
+    private DevelopmentCard Yellow33 = new DevelopmentCard (new int[]{0, 0, 0, 7}, new int[]{0, 1, 0, 0}, new int[]{1, 0, 0, 0, 3}, 40, DevelopmentCard.colorCard.Yellow, 3, 11);
+    private DevelopmentCard Yellow34 = new DevelopmentCard (new int[]{4, 0, 0, 4}, new int[]{0, 1, 0, 0}, new int[]{3, 0, 0, 1, 0}, 41, DevelopmentCard.colorCard.Yellow, 3, 12);
 
-    private DevelopmentCard Purple31 = new DevelopmentCard (new int[]{2, 0, 0, 0}, new int[]{0, 0, 0, 1}, new int[]{0, 0, 0, 0, 1}, 42, DevelopmentCard.colorCard.Purple, 3, 9);
-    private DevelopmentCard Purple32 = new DevelopmentCard (new int[]{1, 1, 1, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 1, 0, 0, 0}, 43, DevelopmentCard.colorCard.Purple, 3, 10);
-    private DevelopmentCard Purple33 = new DevelopmentCard (new int[]{3, 0, 0, 0}, new int[]{0, 0, 2, 0}, new int[]{1, 1, 0, 1, 0}, 44, DevelopmentCard.colorCard.Purple, 3, 11);
-    private DevelopmentCard Purple34 = new DevelopmentCard (new int[]{2, 0, 0, 2}, new int[]{0, 1, 1, 0}, new int[]{0, 0, 0, 2, 1}, 45, DevelopmentCard.colorCard.Purple, 3, 12);
+    private DevelopmentCard Purple31 = new DevelopmentCard (new int[]{6, 0, 0, 0}, new int[]{0, 0, 0, 2}, new int[]{0, 0, 3, 0, 2}, 42, DevelopmentCard.colorCard.Purple, 3, 9);
+    private DevelopmentCard Purple32 = new DevelopmentCard (new int[]{5, 0, 2, 0}, new int[]{0, 1, 0, 1}, new int[]{2, 0, 2, 0, 1}, 43, DevelopmentCard.colorCard.Purple, 3, 10);
+    private DevelopmentCard Purple33 = new DevelopmentCard (new int[]{7, 0, 0, 0}, new int[]{0, 0, 1, 0}, new int[]{0, 0, 0, 1, 3}, 44, DevelopmentCard.colorCard.Purple, 3, 11);
+    private DevelopmentCard Purple34 = new DevelopmentCard (new int[]{4, 4, 0, 0}, new int[]{0, 0, 1, 0}, new int[]{1, 0, 0, 3, 0}, 45, DevelopmentCard.colorCard.Purple, 3, 12);
 
-    private DevelopmentCard Blue31 = new DevelopmentCard (new int[]{0, 0, 2, 0}, new int[]{0, 1, 0, 0}, new int[]{0, 0, 0, 0, 1}, 46, DevelopmentCard.colorCard.Blue, 3, 9);
-    private DevelopmentCard Blue32 = new DevelopmentCard (new int[]{1, 0, 1, 1}, new int[]{1, 0, 0, 0}, new int[]{0, 0, 0, 1, 0}, 47, DevelopmentCard.colorCard.Blue, 3, 10);
-    private DevelopmentCard Blue33 = new DevelopmentCard (new int[]{0, 0, 3, 0}, new int[]{0, 0, 0, 2}, new int[]{1, 1, 1, 0, 0}, 48, DevelopmentCard.colorCard.Blue, 3, 11);
-    private DevelopmentCard Blue34 = new DevelopmentCard (new int[]{2, 0, 2, 0}, new int[]{0, 1, 0, 1}, new int[]{2, 0, 0, 0, 1}, 49, DevelopmentCard.colorCard.Blue, 3, 12);
+    private DevelopmentCard Blue31 = new DevelopmentCard (new int[]{0, 0, 6, 0}, new int[]{2, 0, 0, 0}, new int[]{0, 3, 0, 0, 2}, 46, DevelopmentCard.colorCard.Blue, 3, 9);
+    private DevelopmentCard Blue32 = new DevelopmentCard (new int[]{0, 0, 5, 2}, new int[]{0, 1, 1, 0}, new int[]{2, 0, 0, 2, 1}, 47, DevelopmentCard.colorCard.Blue, 3, 10);
+    private DevelopmentCard Blue33 = new DevelopmentCard (new int[]{0, 0, 7, 0}, new int[]{0, 0, 0, 1}, new int[]{0, 1, 0, 0, 3}, 48, DevelopmentCard.colorCard.Blue, 3, 11);
+    private DevelopmentCard Blue34 = new DevelopmentCard (new int[]{0, 0, 4, 4}, new int[]{1, 0, 0, 0}, new int[]{0, 3, 1, 0, 0}, 49, DevelopmentCard.colorCard.Blue, 3, 12);
 }
