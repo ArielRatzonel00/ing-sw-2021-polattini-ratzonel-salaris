893443b31ae60802772fbe782f1b039d11be3b52
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-04-11 13:26:29 +0200

    Bozza di acquisto developmentCard

--- src/main/java/it/polimi/ingsw/Controller/Acquisto.java
+++ src/main/java/it/polimi/ingsw/Controller/Acquisto.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.Controller;
+import it.polimi.ingsw.Model.*;
 
 public class Acquisto {
+}
+Slots playerSlot = new Slots();
+DevelopmentCard currentCard = new DevelopmentCard();
+
+
+
+void acquireCard(Player player, DevelopmentCard card, int n){
+    int costo[];
+playerSlot=player.getSlots();
+currentCard=playerSlot.getCard1Top();
+/*Casistiche:
+topCard è null -> playerslot.setCardTop(card);
+topCard è di livello 3 -> "SLOT NON SELEZIONABILE"
+
+topCard è di livello 1 o 2 ->
+    se card.getlevel == currentCard.getLevel +1
+            costo=currentCard.getCost()
+            creo un NuovoArray in cui sommo le risorse presenti in strongbox e in warehouse
+            controllo se nuovoArray[i] >= di costo[i] per ogni i ->
+                se si -> card diventa topcard e modifico lista dello slot
+                se no -> "NON ABBASTANZA MATERIALE"
+    se no -> "CONTROLLA MEGLIO IL LIVELLO!"
+ */
+
 }
