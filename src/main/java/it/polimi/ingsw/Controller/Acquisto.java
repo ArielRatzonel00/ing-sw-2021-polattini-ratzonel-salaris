6c2b521f3fcf4da79d163e5af2a038917222e923
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-04-13 00:15:26 +0200

    added cost

--- src/main/java/it/polimi/ingsw/Controller/Acquisto.java
+++ src/main/java/it/polimi/ingsw/Controller/Acquisto.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.Controller;
 import it.polimi.ingsw.Model.*;
 
+
 public class Acquisto {
 }
 Slots playerSlot = new Slots();
@@ -8,35 +9,22 @@ DevelopmentCard currentCard = new DevelopmentCard();
 
 
 
-void acquireCard(Player player, DevelopmentCard card, int n){
-    int costo[];
+void acquireCard(Player player, DevelopmentCard card, int n, int resoucesFromStrongox[], int resourcesFromWarehouse[]){
+    int selectedResources[4]=somma(resourcesFromWarehouse,resoucesFromStrongox);
+    //controllo che player abbia effettivamente le risorse in strongbox e Warehouse.
 playerSlot=player.getSlots();
 if(n==1) {
     currentCard = playerSlot.getCard1Top();
-    playerSlot.setCard1();
+    playerSlot.addSlot1(card);
 }
 if(n==2) {
     currentCard = playerSlot.getCard2Top(); //Da riguardare questa parte
-    playerSlot.setCard1();
+    playerSlot.addSlot2(card);
 }
 if(n==3) {
     currentCard = playerSlot.getCard3Top();
-    playerSlot.setCard1();
+    playerSlot.addSlot2(card);
 }
 
-/*Casistiche:
-topCard è null -> playerslot.setCardTop(card); //slot N
-
-topCard è di livello 3 -> "SLOT NON SELEZIONABILE"
-
-topCard è di livello 1 o 2 ->
-    se card.getlevel == currentCard.getLevel +1
-            costo=currentCard.getCost()
-            creo un NuovoArray in cui sommo le risorse presenti in strongbox e in warehouse
-            controllo se nuovoArray[i] >= di costo[i] per ogni i ->
-                se si -> card diventa topcard, modifica lista dello slot, set warehouse, set strongbox.
-                se no -> "NON ABBASTANZA MATERIALE"
-    se no -> "CONTROLLA MEGLIO IL LIVELLO!"
- */
 
 }
