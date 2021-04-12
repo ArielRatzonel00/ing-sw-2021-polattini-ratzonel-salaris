d4eaa148a3ef135f32cf0d9f638ea5a8818fee60
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-04-12 19:20:20 +0200

    slots class renewed with ArrayLists

--- src/main/java/it/polimi/ingsw/Model/Slots.java
+++ src/main/java/it/polimi/ingsw/Model/Slots.java
@@ -1,55 +1,109 @@
 package it.polimi.ingsw.Model;
-import java.util.arrayList;
+import java.util.ArrayList;
 
 public class Slots {
-    private arrayList<DevelopmentCard> slot1; // possiamo crearlo di 3 posti questo array? // possiamo farlo direttamente con gli id?
-    private arrayList <DevelopmentCard> slot2;
-    private arrayList <DevelopmentCard> slot3;
+    private ArrayList<DevelopmentCard> slot1; // possiamo crearlo di 3 posti questo array? // possiamo farlo direttamente con gli id?
+    private ArrayList<DevelopmentCard> slot2;
+    private ArrayList<DevelopmentCard> slot3;
 
     private int[] ColorDevCards = {0, 0, 0, 0}; //posizione 0: purple, 1=blue, 2=yellow, 3=green
 
-    public <DevelopmentCard>
 
-    public arrayList<DevelopmentCard> getSlot1() {
+    public ArrayList<DevelopmentCard> getSlot1() {
         return slot1;
     }
 
-    public DevelopmentCard[] getCard2() {
+    public ArrayList<DevelopmentCard> getSlot2() {
         return slot2;
     }
 
-    public DevelopmentCard[] getCard3() {
+    public ArrayList<DevelopmentCard> getSlot3() {
         return slot3;
     }
 
+
     public DevelopmentCard getCard1Top() {
-        return slot1[0]; // verificare che sia effettivamente la 0 la più sopra
+        return (slot1.get(slot1.size() - 1));
     }
 
     public DevelopmentCard getCard2Top() {
-        return slot2[0]; // verificare che sia effettivamente la 0 la più sopra
+        return (slot1.get(slot2.size() - 1));
     }
 
     public DevelopmentCard getCard3Top() {
-        return slot3[0]; // verificare che sia effettivamente la 0 la più sopra
+        return (slot1.get(slot3.size() - 1));
     }
 
 
-    public void setCard1(DevelopmentCard card1) {
-        if(card1.getLevel()>=getCard1Top().getLevel())
-        this.slot1 = card1;
+    public void setSlot1(DevelopmentCard card1) {
+        if (card1.getLevel() >= getCard1Top().getLevel())
+            slot1.add(card1);
     }
 
-    public void setCard2(DevelopmentCard[] card2) {
-        this.slot2 = card2;
+    public void setSlot2(DevelopmentCard card1) {
+        if (card1.getLevel() >= getCard1Top().getLevel())
+            slot1.add(card1);
     }
 
-    public void setCard3(DevelopmentCard[] card3) {
-        this.slot3 = card3;
+    public void setSlot3(DevelopmentCard card1) {
+        if (card1.getLevel() >= getCard1Top().getLevel())
+            slot1.add(card1);
     }
 
-    public int[] getNumberOfDevCardsForColors(){ //da controllare bene
-       for(int counter=0; counter < slot1.length; counter++)
+    public int[] getNumberOfDevCardsForColors() { //da controllare bene
+        for (DevelopmentCard card : slot1) {
+            switch (card.getColor()) {
+
+                case Purple:
+                    ColorDevCards[0]++;
+                    break;
+                case Blue:
+                    ColorDevCards[1]++;
+                    break;
+                case Green:
+                    ColorDevCards[2]++;
+                    break;
+                case Yellow:
+                    ColorDevCards[3]++;
+                    break;
+            }
+        }
+        for (DevelopmentCard card : slot2) {
+            switch (card.getColor()) {
+
+                case Purple:
+                    ColorDevCards[0]++;
+                    break;
+                case Blue:
+                    ColorDevCards[1]++;
+                    break;
+                case Green:
+                    ColorDevCards[2]++;
+                    break;
+                case Yellow:
+                    ColorDevCards[3]++;
+                    break;
+            }
+        }
+        for (DevelopmentCard card : slot3) {
+            switch (card.getColor()) {
+
+                case Purple:
+                    ColorDevCards[0]++;
+                    break;
+                case Blue:
+                    ColorDevCards[1]++;
+                    break;
+                case Green:
+                    ColorDevCards[2]++;
+                    break;
+                case Yellow:
+                    ColorDevCards[3]++;
+                    break;
+            }
+        }
+    /*
+        for(int counter=0; counter < slot1.size(); counter++)
        {
           if(slot1[counter].getColor() == DevelopmentCard.colorCard.Purple){
               ColorDevCards[0]+=1;
@@ -71,6 +125,7 @@ public class Slots {
 
           }
        }
+
         for(int c=0; c < slot2.length; c++)
         {
             if(slot2[c].getColor() == DevelopmentCard.colorCard.Purple){
@@ -114,14 +169,14 @@ public class Slots {
                 }
 
             }
-        }
-       return ColorDevCards;
+        } */
+        return ColorDevCards;
     }
 
-}
    /* public void addCard(DevelopmentCard developmentCard){
 
     }
 }
 
     */
+}
