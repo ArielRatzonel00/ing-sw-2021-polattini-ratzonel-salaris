62110ec3959ae4812da49a8a6bc63052e340c570
LucaPolattini <luca.polattini@mail.polimi.it>
2021-04-14 23:57:51 +0200

    Impostazione Production

--- src/main/java/it/polimi/ingsw/Controller/Production.java
+++ src/main/java/it/polimi/ingsw/Controller/Production.java
@@ -1,15 +1,40 @@
 package it.polimi.ingsw.Controller;
 
 import it.polimi.ingsw.Model.DevelopmentCard;
+import it.polimi.ingsw.Model.LeaderCard;
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Model.Slots;
 
-public class Production (Player player) {
-    private DevelopmentCard Card1Production = player.getSlots().getCard1Top();
-    private DevelopmentCard Card2Production = player.getSlots().getCard2Top();;
-    private DevelopmentCard Card3Production = player.getSlots().getCard3Top();;
-    private LeaderCardController Card4Production;
-    private LeaderCardController Card5Production;
+public class Production {
+
+    private Player player;
+
+    public Production(Player player) {
+        this.player = player;
+    }
+
+    private boolean act1 =false;
+    private boolean act2 =false;
+    private boolean act3 =false;
+    private boolean act4 =false;
+    private boolean act5 =false;
+
+    private int[] TotalResourcesNeed = {0, 0, 0, 0}; //0=purple, 1=blue, 2=yellow, 3=grey
+    private int[] TotalResourcesPoss = {0, 0, 0, 0}; //0=purple, 1=blue, 2=yellow, 3=grey
+
+
+    public void activateProductionDev(int[] WhatCards){
+    // WhatCards 0=slot1Top 1=slot2Top 2=slot3Top 3=Lead1 4=Lead2. Se nella casella tot c'è 1 vuol dire che devo
+        // attivare quella produzione
+        if(WhatCards[0]==1){
+
+        }
+
+
+    }
+
+
+
 
 }
 
