cbeeccbafa5a707e297871398db7b40c164eae4a
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-04-19 15:20:52 +0200

    Added methods in DevCardSlot

--- src/main/java/it/polimi/ingsw/Model/DevCardSlot.java
+++ src/main/java/it/polimi/ingsw/Model/DevCardSlot.java
@@ -1,16 +1,50 @@
 package it.polimi.ingsw.Model;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class DevCardSlot {
     private ArrayList<DevelopmentCard> cards;
 
+    //add card above the top card if its level is higher
     public void addCard(DevelopmentCard card){
         if (card.getLevel() > getTopCard().getLevel())
             cards.add(card);
     }
+
+    //get the "active" card, the one on top of the slot
     public DevelopmentCard getTopCard(){
         return (cards.get(cards.size() - 1));
+    }
+
+    //Count the total victory points of the cards in the slot
+    public int countVictoryPoints(){
+        int points=0;
+        for (DevelopmentCard a: cards ){
+            points+=a.getVictoryPoints();
+        }
+        return points;
+    }
+
+    //return the array of cards
+    public ArrayList<DevelopmentCard> getCards(){
+        return cards;
+    }
+
+    public int filterCount(int level){
+        List<DevelopmentCard> cardsLevelN = cards.stream().filter(c->c.getLevel()==level).collect(Collectors.toList());
+        return cardsLevelN.size();
+    }
+    public int filterCount(DevelopmentCard.colorCard color){
+        List<DevelopmentCard> cardsLevelN = cards.stream().filter(c->c.getColor()==color).collect(Collectors.toList());
+        return cardsLevelN.size();
+    }
+    public int filterCount(DevelopmentCard.colorCard color, int level){
+        List<DevelopmentCard> cardsLevelN = cards.stream().filter(c->((c.getColor()==color)&&(c.getLevel()==level))).collect(Collectors.toList());
+        return cardsLevelN.size();
     }
 
 }
--- src/main/java/it/polimi/ingsw/Model/SlotsBoard.java
+++ src/main/java/it/polimi/ingsw/Model/SlotsBoard.java
@@ -1,11 +1,10 @@
 package it.polimi.ingsw.Model;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SlotsBoard {
     private ArrayList<DevCardSlot> slots;
-
-    private int[] ColorDevCards = {0, 0, 0, 0}; //posizione 0: purple, 1=blue, 2=yellow, 3=green
-    private boolean Respect = false;
 
 
     // Create a SlotBoard with n (number that is set on the rules of the game) initial slots
@@ -15,157 +14,57 @@ public class SlotsBoard {
         }
     }
 
+    //slots getter
+    public ArrayList<DevCardSlot> getSlots() {
+        return slots;
+    }
 
-    // function that add a received slot (useful for LeaderCard)
+    // method that adds a slot received as parameter (useful for LeaderCard)
     public void addSlot(DevCardSlot slot) {
         slots.add(slot);
     }
 
-
-
-
-    // Da rivedere
-
-
-
-
-
-    public boolean getDevCardsLevel2(int colorCard) { //mi ritorna true se ho una carta di quel colore di livello 2 nei miei slot
-        //1: purple, 2=blue, 3=yellow, 4=green
-        if (slot1.size()>0){
-            switch (colorCard) {
-
-                case 1: if(slot1.get(1).getColor().equals(DevelopmentCard.colorCard.Purple)) {
-                    Respect = true;
-                }
-                    break;
-                case 2:
-                    if(slot1.get(1).getColor().equals(DevelopmentCard.colorCard.Blue)) {
-                        Respect = true;
-                    }
-                    break;
-                case 3:
-                    if(slot1.get(1).getColor().equals(DevelopmentCard.colorCard.Yellow)) {
-                        Respect = true;
-                    }
-                    break;
-                case 4:
-                    if(slot1.get(1).getColor().equals(DevelopmentCard.colorCard.Green)) {
-                        Respect = true;
-                    }
-                    break;
-            }
+    //count total victory points from the slots
+    public int countVictoryPoints(){
+            int points=0;
+        for (DevCardSlot a: slots) {
+            points += a.countVictoryPoints();
         }
-        else{
-            if (slot2.size()>0){
-                switch (colorCard) {
-
-                    case 1: if(slot2.get(1).getColor().equals(DevelopmentCard.colorCard.Purple)) {
-                        Respect = true;
-                    }
-                        break;
-                    case 2:
-                        if(slot2.get(1).getColor().equals(DevelopmentCard.colorCard.Blue)) {
-                            Respect = true;
-                        }
-                        break;
-                    case 3:
-                        if(slot2.get(1).getColor().equals(DevelopmentCard.colorCard.Yellow)) {
-                            Respect = true;
-                        }
-                        break;
-                    case 4:
-                        if(slot2.get(1).getColor().equals(DevelopmentCard.colorCard.Green)) {
-                            Respect = true;
-                        }
-                        break;
-                }
-            }
-            else{
-                if (slot3.size()>0){
-                    switch (colorCard) {
-
-                        case 1: if(slot3.get(1).getColor().equals(DevelopmentCard.colorCard.Purple)) {
-                            Respect = true;
-                        }
-                            break;
-                        case 2:
-                            if(slot3.get(1).getColor().equals(DevelopmentCard.colorCard.Blue)) {
-                                Respect = true;
-                            }
-                            break;
-                        case 3:
-                            if(slot3.get(1).getColor().equals(DevelopmentCard.colorCard.Yellow)) {
-                                Respect = true;
-                            }
-                            break;
-                        case 4:
-                            if(slot3.get(1).getColor().equals(DevelopmentCard.colorCard.Green)) {
-                                Respect = true;
-                            }
-                            break;
-                    }
-                }
-            }
-
-        }
-        return Respect;
+        return points;
     }
 
-    public int[] getNumberOfDevCardsForColors() { //da controllare bene
-        for (DevelopmentCard card : slot1) {
-            switch (card.getColor()) {
-
-                case Purple:
-                    ColorDevCards[0]++;
-                    break;
-                case Blue:
-                    ColorDevCards[1]++;
-                    break;
-                case Green:
-                    ColorDevCards[2]++;
-                    break;
-                case Yellow:
-                    ColorDevCards[3]++;
-                    break;
-            }
+    //Get total number of DevelopmentCards on a player board, useful for the 7 cards ending game check
+    public int getCardsQuantity(){
+        int cards=0;
+        for (DevCardSlot a: slots) {
+            cards += a.getCards().size();
         }
-        for (DevelopmentCard card : slot2) {
-            switch (card.getColor()) {
-
-                case Purple:
-                    ColorDevCards[0]++;
-                    break;
-                case Blue:
-                    ColorDevCards[1]++;
-                    break;
-                case Green:
-                    ColorDevCards[2]++;
-                    break;
-                case Yellow:
-                    ColorDevCards[3]++;
-                    break;
-            }
-        }
-        for (DevelopmentCard card : slot3) {
-            switch (card.getColor()) {
-
-                case Purple:
-                    ColorDevCards[0]++;
-                    break;
-                case Blue:
-                    ColorDevCards[1]++;
-                    break;
-                case Green:
-                    ColorDevCards[2]++;
-                    break;
-                case Yellow:
-                    ColorDevCards[3]++;
-                    break;
-            }
-        }
-
-        return ColorDevCards;
+        return cards;
     }
+
+    // filterCount receive a level, a color or a pair of level and color and return the total number of DevelopmentCards in the SlotsBoard with that attributes
+
+    public int filterCount(int level){
+        int total=0;
+        for (DevCardSlot a: slots) {
+            total += a.filterCount(level);
+        }
+        return total;
+    }
+    public int filterCount(DevelopmentCard.colorCard color){
+        int total=0;
+        for (DevCardSlot a: slots) {
+            total += a.filterCount(color);
+        }
+        return total;
+    }
+    public int filterCount(DevelopmentCard.colorCard color, int level){
+        int total=0;
+        for (DevCardSlot a: slots) {
+            total += a.filterCount(color,level);
+        }
+        return total;
+    }
+
 
 }
