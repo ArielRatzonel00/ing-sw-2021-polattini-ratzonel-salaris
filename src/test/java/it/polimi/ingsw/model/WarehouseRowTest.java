34322f7a45cd60c30f355ebe2dfad02782c85c80
LucaPolattini <luca.polattini@mail.polimi.it>
2021-04-27 22:36:52 +0200

    Test

--- src/test/java/it/polimi/ingsw/model/WarehouseRowTest.java
+++ src/test/java/it/polimi/ingsw/model/WarehouseRowTest.java
@@ -1,14 +1,11 @@
 package it.polimi.ingsw.model;
 
-import it.polimi.ingsw.Model.Deck;
-import it.polimi.ingsw.Model.DevCardSlot;
-import it.polimi.ingsw.Model.GridCell;
 import it.polimi.ingsw.Model.Marble.MarketMarble;
 import it.polimi.ingsw.Model.WarehouseRow;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseRowTest {
@@ -19,6 +16,7 @@ class WarehouseRowTest {
     private MarketMarble Yellow;
     private MarketMarble Purple;
     private MarketMarble Blue;
+    private ArrayList<MarketMarble> marbles;
 
     @BeforeEach
     void initialization(){
@@ -34,32 +32,33 @@ class WarehouseRowTest {
     @Test
     @DisplayName("Adding marbles to rows")
     void addMarble() {
-       assertTrue(Row1.addMarble(Grey), "La grey è in Row1 -> 1");
-       assertFalse(Row1.addMarble(Grey), "La grey è in Row1 -> 2");
-       assertTrue(Row2.addMarble(Grey), "La grey è in Row2-> 1"); //posso aggiungerla perchè il controllo è in warehouse
+        assertTrue(Row1.addMarble(Grey), "La grey è in Row1 -> 1");
+        assertFalse(Row1.addMarble(Grey), "La grey è in Row1 -> 2");
+        assertTrue(Row2.addMarble(Grey), "La grey è in Row2-> 1");//posso aggiungerla perchè il controllo è in warehouse
+        assertFalse(Row2.addMarble(Blue), "La blu è in Row2-> 1");//la blu non può andare in row2 perchè c'è già una grigia che blocca il colore
+        assertTrue(Row2.addMarble(Grey), "La grey è in Row2-> 2");
+        assertFalse(Row2.addMarble(Grey), "La grey è in Row2-> 3");//la row2 ha solo 2 posti
+        assertTrue(Row3.addMarble(Grey), "La grey è in Row3 -> 1");
+        assertTrue(Row3.addMarble(Grey), "La grey è in Row3 -> 2");
+        assertTrue(Row3.addMarble(Grey), "La grey è in Row3 -> 3");
+        assertFalse(Row3.addMarble(Grey), "La grey è in Row3 -> 4");//no perchè spazio pieno
     }
 
     @Test
+    @DisplayName("Remove marbles from rows")
     void removeMarble() {
+        assertFalse(Row1.removeMarble(Grey),"La grey non c'è quindi non può essere rimossa");
+        Row1.addMarble(Grey);
+        assertTrue(Row1.removeMarble(Grey),"La grey è rimossa");
+        Row1.addMarble(Grey);
+        Row2.addMarble(Grey);
+        Row2.addMarble(Grey);
+        assertTrue(Row2.removeMarble(Grey),"La grey è rimossa");
+        assertFalse(Row2.removeMarble(Blue),"La blu non esiste");
     }
 
     @Test
-    void getMarbles() {
-    }
+    void changeMarbles() { //i controlli avvengono in warehouse quindi inutile testare qui
 
-    @Test
-    void changeMarbles() {
-    }
-
-    @Test
-    void getSpace() {
-    }
-
-    @Test
-    void getColor() {
-    }
-
-    @Test
-    void setColor() {
     }
 }
