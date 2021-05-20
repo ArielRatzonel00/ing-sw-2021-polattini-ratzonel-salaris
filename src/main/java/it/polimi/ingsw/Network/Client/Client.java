2cadff9c9811463ba1832e083996e48e9ce14b16
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-05-20 17:59:07 +0200

    Client

--- src/main/java/it/polimi/ingsw/Network/Client/Client.java
+++ src/main/java/it/polimi/ingsw/Network/Client/Client.java
@@ -1,21 +1,34 @@
 package it.polimi.ingsw.Network.Client;
 
+import it.polimi.ingsw.Network.Messages.Message;
+import it.polimi.ingsw.Network.Messages.Messanger;
+import it.polimi.ingsw.Network.Messages.SocketMessage;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class Client {
+public class Client extends Messanger {
 
     private String ip;
     private int port;
+    private int ID=1;
+    private ObjectOutputStream socketOut;
+    private ObjectInputStream socketIn;
+    private ClientView clientView;
 
     public Client(String ip, int port) {
         this.ip = ip;
         this.port = port;
         
+    }
+
+    public void setClientView(ClientView clientView) {
+        this.clientView = clientView;
     }
 
     private boolean active = true;
@@ -26,6 +39,22 @@ public class Client {
 
     public synchronized void setActive(boolean active) {
         this.active = active;
+    }
+    public synchronized void receiveMessage(SocketMessage message) throws IOException {
+        if(message.getReceiver()==null || message.getReceiver().contains(this.ID)){
+            System.out.println("entra nello switch");
+
+            switch (message.getID()){
+                case "abc":
+                    System.out.println("funziona");
+                    break;
+                case "id:1":
+                    this.ID=1;
+                    sendMessage(socketOut, new SocketMessage("2Players",null));
+                default:
+                    break;
+            }
+        }
     }
 
     /*public Thread asyncReadFromSocket(final ObjectInputStream socketIn) {
@@ -71,23 +100,21 @@ public class Client {
         return t;
     }
 */
+
     public void run() throws IOException {
         Socket socket = new Socket(ip, port);
         System.out.println("Connection established");
-        ObjectInputStream socketIn = new ObjectInputStream(socket.getInputStream());
-        PrintWriter socketOut = new PrintWriter(socket.getOutputStream());
+        socketOut = new ObjectOutputStream(socket.getOutputStream());
+        socketIn=new ObjectInputStream(socket.getInputStream());
         Scanner stdin = new Scanner(System.in);
 
             try {
-                while (true) {
-                    System.out.println(socketIn.readObject());
-                    socketOut.println(stdin.nextLine());
-                    socketOut.flush();
-                /*Thread t0 = asyncReadFromSocket(socketIn);
-                Thread t1 = asyncWriteToSocket(stdin, socketOut);
-                t0.join();
-                t1.join();*/
-                }
+                System.out.println("Quanto vuoi che valga A?");
+                int a=stdin.nextInt();
+                socketOut.writeObject(a);
+
+                SocketMessage b= (SocketMessage)socketIn.readObject();
+                receiveMessage(b);
             } catch (NoSuchElementException | ClassNotFoundException e) {
                 System.out.println("Connection closed from the client side");
             } finally {
