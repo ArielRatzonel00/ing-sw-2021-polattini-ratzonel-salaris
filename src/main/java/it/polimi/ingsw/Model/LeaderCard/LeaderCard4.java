00bb97b4d8cf0d2a79993f4bf54c8144561668ca
Matias Salaris <matiasgiovanni.salaris@mail.polimi.it>
2021-05-26 11:39:08 +0200

    ClientModel

--- src/main/java/it/polimi/ingsw/Model/LeaderCard/LeaderCard4.java
+++ src/main/java/it/polimi/ingsw/Model/LeaderCard/LeaderCard4.java
@@ -7,11 +7,13 @@ import it.polimi.ingsw.Model.Production;
 
 
 public class LeaderCard4 extends LeaderCard { //converte in punto fede e materia a caso -> produzione extra
+    private int type;
     private DevelopmentCard.colorCard CostCardLevelTwo;
     private Production production;
 
     public LeaderCard4(int id, DevelopmentCard.colorCard CostCardLevelTwo, int victoryPoints, Production production) {
         super(id, victoryPoints);
+        type=4;
         this.CostCardLevelTwo = CostCardLevelTwo;
         this.production = production;
     }
@@ -33,10 +35,22 @@ public class LeaderCard4 extends LeaderCard { //converte in punto fede e materia
     }
 
     @Override
+    public String toString() {
+        return "LeaderCard4{" +
+                "victoryPoints=" + getVictoryPoints() +
+                ", CostaCardLevelTwo="+getCostCardLevelTwo() +
+                ",production="+getProduction()+
+                '}';
+    }
+    @Override
     public String StampaCarta() {
         return "POTERE: Aggiunge produzione da" + getProduction().getProductionCost().get(0).getCostNumber() + getProduction().getProductionCost().get(0).getCostColor() + " ad un punto fede e una risorsa a tua scelta\n " +
                 "NECESSITA: DevCard di livello 2 di colore" + getCostCardLevelTwo() + "\n" +
                 "VICTORY POINTS: " + getVictoryPoints();
+    }
+    @Override
+    public int getType() {
+        return 4;
     }
 }
 //
