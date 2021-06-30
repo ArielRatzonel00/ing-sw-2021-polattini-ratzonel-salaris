bb44e6c27a5c5bfa17b05adf3ce32ea275dafe8c
LucaPolattini <luca.polattini@mail.polimi.it>
2021-06-30 19:22:55 +0200

    Test

--- src/test/java/it/polimi/ingsw/Model/ModelTest.java
+++ src/test/java/it/polimi/ingsw/Model/ModelTest.java
@@ -12,15 +12,19 @@ class ModelTest {
     Player player1, player2, player3, player4;
     Model model, modelsingle;
     ArrayList<Player> players, playersingle;
-    int lead0,lead1,lead2,lead3;
+    int lead0, lead1, lead2, lead3;
     CostOfCard cost0, cost1, cost2, cost3;
     CostOfCard cost4, cost5, cost6, cost7;
     DevelopmentCard card1;
     MarketMarble yellow, purple, blue, grey;
     Deck deck;
+    PopeFavorState popeFavorAct;
+    PopeFavorState popeFavorDel;
+    PopeFavorState popeFavorUn;
+
 
     @BeforeEach
-    void initialization(){
+    void initialization() {
         player1 = new Player("Luca");
         player2 = new Player("Marco");
         player3 = new Player("Gianfranco");
@@ -47,6 +51,9 @@ class ModelTest {
         purple = new MarketMarble(MarketMarble.ColorMarble.PURPLE);
         grey = new MarketMarble(MarketMarble.ColorMarble.GREY);
         deck = new Deck();
+        popeFavorAct = PopeFavorState.Activate;
+        popeFavorDel = PopeFavorState.Deleted;
+        popeFavorUn = PopeFavorState.Unabled;
     }
 
     @Test
@@ -57,17 +64,17 @@ class ModelTest {
         lead1 = model.getPlayers().get(0).getLeaderCard(1).getId();
         lead2 = model.getPlayers().get(0).getLeaderCard(2).getId();
         lead3 = model.getPlayers().get(0).getLeaderCard(3).getId();
-        model.DiscardInitialLeaderCards(0,1, 2);
-        assertSame(lead0,model.getPlayers().get(0).getLeaderCard(0).getId());
-        assertSame(lead2,model.getPlayers().get(0).getLeaderCard(1).getId());
+        model.DiscardInitialLeaderCards(0, 1, 2);
+        assertSame(lead0, model.getPlayers().get(0).getLeaderCard(0).getId());
+        assertSame(lead2, model.getPlayers().get(0).getLeaderCard(1).getId());
     }
 
     @Test
     void setInitialResourcesTest() {
         model.SetInitialResourcesForFirstPlayer();
-        model.SetInitialResourcesForSecondPlayer(MarketMarble.ColorMarble.BLUE,2);
-        model.SetInitialResourcesForThirdPlayer(MarketMarble.ColorMarble.GREY,1);
-        model.SetInitialResourcesForForthPlayer(MarketMarble.ColorMarble.GREY,1, MarketMarble.ColorMarble.GREY,1);
+        model.SetInitialResourcesForSecondPlayer(MarketMarble.ColorMarble.BLUE, 2);
+        model.SetInitialResourcesForThirdPlayer(MarketMarble.ColorMarble.GREY, 1);
+        model.SetInitialResourcesForForthPlayer(MarketMarble.ColorMarble.GREY, 1, MarketMarble.ColorMarble.GREY, 1);
         assertEquals(0, model.getPlayers().get(0).getWarehouse().getNumberOfResource(MarketMarble.ColorMarble.BLUE));
         assertEquals(1, model.getPlayers().get(1).getWarehouse().getNumberOfResource(MarketMarble.ColorMarble.BLUE));
         assertEquals(1, model.getPlayers().get(2).getWarehouse().getNumberOfResource(MarketMarble.ColorMarble.GREY));
@@ -81,8 +88,8 @@ class ModelTest {
         marketTray.ShiftMatrixByCol(2);
         MarketMarble[] returnedMarblesTest1 = new MarketMarble[3];
         returnedMarblesTest1 = model.getMarketTray().GetMarketMarblesFromCol(2);
-        model.MarketTrayAction(0,true,2);
-        assertSame(model.getMarketTray().getMarketMatrix(),marketTray.getMarketMatrix());
+        model.MarketTrayAction(0, true, 2);
+        assertSame(model.getMarketTray().getMarketMatrix(), marketTray.getMarketMatrix());
     }
 
     @Test
@@ -99,7 +106,7 @@ class ModelTest {
         Rows.add(0);
         Rows.add(1);
         Rows.add(2);
-        model.DealWithResources(0,keep,marbles,Rows);
+        model.DealWithResources(0, keep, marbles, Rows);
         assertEquals(1, model.getPlayers().get(0).getWarehouse().getNumberOfTotalResourcesInWarehouse());
         marbles.remove(2);
         keep.remove(2);
@@ -116,7 +123,7 @@ class ModelTest {
         model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.YELLOW);
         model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.GREY);
         model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.PURPLE);
-        model.WantToBuyCard(0,0,0,0);
+        model.WantToBuyCard(0, 0, 0, 0);
         ArrayList<CostOfCard> costSt = new ArrayList<>();
         costSt.add(cost0);
         costSt.add(cost1);
@@ -133,9 +140,9 @@ class ModelTest {
         Rows.add(2);
         Rows.add(2);
 
-        card1 = model.getDevelopmentGrid().get(0,0);
-        model.BuyCard(0,costSt,costWh,Rows,0,0,0);
-        assertSame(card1,model.getPlayers().get(0).getSlotsBoard().getSlots().get(0).getTopCard());
+        card1 = model.getDevelopmentGrid().get(0, 0);
+        model.BuyCard(0, costSt, costWh, Rows, 0, 0, 0);
+        assertSame(card1, model.getPlayers().get(0).getSlotsBoard().getSlots().get(0).getTopCard());
     }
 
     @Test
@@ -145,10 +152,10 @@ class ModelTest {
         lead1 = model.getPlayers().get(0).getLeaderCard(1).getId();
         lead2 = model.getPlayers().get(0).getLeaderCard(2).getId();
         lead3 = model.getPlayers().get(0).getLeaderCard(3).getId();
-        model.DiscardInitialLeaderCards(0,1, 2);
-        model.DiscardLeaderCardAction(0,1);
+        model.DiscardInitialLeaderCards(0, 1, 2);
+        model.DiscardLeaderCardAction(0, 1);
 
-        assertSame(lead0,model.getPlayers().get(0).getLeaderCard(0).getId());
+        assertSame(lead0, model.getPlayers().get(0).getLeaderCard(0).getId());
     }
 
     @Test
@@ -162,10 +169,10 @@ class ModelTest {
         lead1 = model.getPlayers().get(0).getLeaderCard(1).getId();
         lead2 = model.getPlayers().get(0).getLeaderCard(2).getId();
         lead3 = model.getPlayers().get(0).getLeaderCard(3).getId();
-        model.DiscardInitialLeaderCards(0,1, 2);
+        model.DiscardInitialLeaderCards(0, 1, 2);
         assertFalse(model.getPlayers().get(0).getLeaderCard(0).isActivate());
-        model.ActivateLeaderCardAction(0,0);
-        if(model.getPlayers().get(0).getLeaderCard(0).canBeActivated(player1)){
+        model.ActivateLeaderCardAction(0, 0);
+        if (model.getPlayers().get(0).getLeaderCard(0).canBeActivated(player1)) {
             assertTrue(model.getPlayers().get(0).getLeaderCards().get(0).isActivate());
         } else {
             assertFalse(model.getPlayers().get(0).getLeaderCards().get(0).isActivate());
@@ -174,29 +181,96 @@ class ModelTest {
 
     @Test
     void moveResourcesTest() {
-        model.getPlayers().get(0).getWarehouse().addToRow(grey,0);
-        model.getPlayers().get(0).getWarehouse().addToRow(blue,1);
+        model.getPlayers().get(0).getWarehouse().addToRow(grey, 0);
+        model.getPlayers().get(0).getWarehouse().addToRow(blue, 1);
         assertSame(MarketMarble.ColorMarble.GREY, model.getPlayers().get(0).getWarehouse().getRow(0).getColor());
         assertSame(MarketMarble.ColorMarble.BLUE, model.getPlayers().get(0).getWarehouse().getRow(1).getColor());
-        model.MoveResources(0,0,1);
+        model.MoveResources(0, 0, 1);
+        assertEquals(1, model.getPlayers().get(0).getWarehouse().getRow(0).getMarbles().size());
         assertSame(MarketMarble.ColorMarble.GREY, model.getPlayers().get(0).getWarehouse().getRow(1).getColor());
-        //assertEquals(2, model.getPlayers().get(0).getWarehouse().getNumberOfTotalResourcesInWarehouse());
+        assertSame(MarketMarble.ColorMarble.BLUE, model.getPlayers().get(0).getWarehouse().getRow(0).getColor());
     }
 
     @Test
     void produceAndCanProduceTest() {
-        model.getPlayers().get(0).getSlotsBoard().getSlots().get(0).addCard(deck.getGreen11());
-        model.getPlayers().get(0).getSlotsBoard().getSlots().get(1).addCard(deck.getGreen12());
+        //faccio la produzione base e la produzione della carta Purple13 -> carta di livello 1 viola con 3 punti vittoria
+        model.getPlayers().get(0).getSlotsBoard().getSlots().get(0).addCard(deck.getPurple13());
+        assertTrue(deck.getPurple13().equals(model.getPlayers().get(0).getSlotsBoard().getSlots().get(0).getTopCard()));
+
+        //rifornisco lo strongbox
+        model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.BLUE);
+        model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.YELLOW);
+        model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.GREY);
+        model.getPlayers().get(0).getStrongbox().AddResource(10, MarketMarble.ColorMarble.PURPLE);
+        assertEquals(40,model.getPlayers().get(0).getStrongbox().getNumberOfTotalResourcesInStrongbox());
+
+        //rifornisco il warehouse
+        model.getPlayers().get(0).getWarehouse().addToRow(blue,0);
+        model.getPlayers().get(0).getWarehouse().addToRow(yellow,1);
+        model.getPlayers().get(0).getWarehouse().addToRow(yellow,1);
+        assertTrue(model.getPlayers().get(0).getWarehouse().CheckRow(1,2, MarketMarble.ColorMarble.YELLOW));
+        assertTrue(model.getPlayers().get(0).getWarehouse().CheckRow(0,1, MarketMarble.ColorMarble.BLUE));
+
+        //scelgo le produzioni da attivare -> la basic e la carta viola
         ArrayList<Integer> productionsIndexes = new ArrayList<>();
+        productionsIndexes.add(0);
+        productionsIndexes.add(1);
+
+        //scelgo la basic
         ArrayList<CostOfCard> productionBasicCost = new ArrayList<>();
-        //
+        CostOfCard costBasic1  = new CostOfCard(1, MarketMarble.ColorMarble.BLUE);
+        CostOfCard costBasic2  = new CostOfCard(1, MarketMarble.ColorMarble.YELLOW);
+        productionBasicCost.add(costBasic1);
+        productionBasicCost.add(costBasic2);
+
+        //controllo di poter produrre
+        model.CanProduce(0, productionsIndexes, productionBasicCost);
+
+        ArrayList<CostOfCard> resources = new ArrayList<>();
+
+
+        ArrayList<ArrayList<CostOfCard>> ResourcesFromStrongbox = new ArrayList<>();
+        ResourcesFromStrongbox.add(resources);
+        ArrayList<CostOfCard> productionPurple = new ArrayList<>();
+        CostOfCard costProd = new CostOfCard(2, MarketMarble.ColorMarble.YELLOW);
+        productionPurple.add(costProd);
+        ResourcesFromStrongbox.add(productionPurple);
+
+        ArrayList<ArrayList<CostOfCard>> ResourcesFromWarehouse = new ArrayList<>();
+
+        ResourcesFromWarehouse.add(productionBasicCost);
+        ResourcesFromWarehouse.add(resources);
+
+        ArrayList<MarketMarble.ColorMarble> profit = new ArrayList<>();
+        profit.add(MarketMarble.ColorMarble.PURPLE);
+
+
+        ArrayList<Integer> Rows = new ArrayList<>();
+        Rows.add(0);
+        Rows.add(1);
+
+        ArrayList<ArrayList<Integer>> rows = new ArrayList<>();
+        rows.add(Rows);
+
+
+        model.Produce(0, productionsIndexes, ResourcesFromStrongbox, ResourcesFromWarehouse, rows, profit);
+
+
     }
 
     @Test
-    void endTurn() {
+    void endTurn1() {
         model.EndTurn(0);
         modelsingle.EndTurn(0);
-        //
+        assertFalse(model.getPlayers().get(0).isYourTurn());
+        assertTrue(model.getPlayers().get(1).isYourTurn());
+    }
+
+    @Test
+    void endTurnLastPlayer() {
+        model.EndTurn(3);
+        assertFalse(model.getPlayers().get(3).isYourTurn());
+        assertTrue(model.getPlayers().get(0).isYourTurn());
     }
 
     @Test
@@ -207,13 +281,38 @@ class ModelTest {
 
     @Test
     void checkPopeFavorState() {
-    }
-
-    @Test
-    void getPopeFavorStates() {
-    }
-
-    @Test
-    void checkPopeFavorStateActivatedByCurrentPlayer() {
+        ArrayList<PopeFavorState> popeFavorStatesUn = new ArrayList<>();
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        popeFavorStatesUn.add(popeFavorUn);
+        assertTrue(popeFavorStatesUn.equals(model.getPopeFavorStates()));
+        model.getPlayers().get(0).getFaithTrack().setRedPosition(8);
+        model.getPlayers().get(1).getFaithTrack().setRedPosition(4);
+        model.getPlayers().get(2).getFaithTrack().setRedPosition(5);
+        model.CheckPopeFavorStateActivatedByCurrentPlayer(1);
+        ArrayList<PopeFavorState> popeFavorStates = new ArrayList<>();
+        popeFavorStates.add(popeFavorAct);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorDel);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorAct);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorDel);
+        popeFavorStates.add(popeFavorUn);
+        popeFavorStates.add(popeFavorUn);
+        model.CheckPopeFavorState(1);
+        assertTrue(popeFavorStates.equals(model.getPopeFavorStates()));
     }
 }
